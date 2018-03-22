df8b43b3685d795141db5f1b63ccc3ecd22f822e
unknown <jacobdillow1@gmail.com>
2018-03-22 15:29:09 -0500

    start

---
+++ test.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+
+master = Tk()
+
+Label(master, text="username").grid(row=0)
+Label(master, text="password").grid(row=1)
+
+e1 = Entry(master)
+e2 = Entry(master,show="*")
+
+e1.grid(row=0,column=1)
+e2.grid(row=1,column=1)
+mainloop()

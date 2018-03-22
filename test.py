95d629205b287175f1850135addcfa6140e0d430
unknown <jacobdillow1@gmail.com>
2018-03-22 16:05:08 -0500

    setting up window

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 
 from tkinter import *
-
+def getinfo():
+    print(e1.get())
+    print(e2.get())
 master = Tk()
-
+master.title = "Test"
+master.geometry('300x100')
 Label(master, text="username").grid(row=0)
 Label(master, text="password").grid(row=1)
 
@@ -11,5 +14,7 @@ e2 = Entry(master,show="*")
 
 e1.grid(row=0,column=1)
 e2.grid(row=1,column=1)
-print('test')
+
+Button(master,text="set",command=getinfo).grid(row=2,column=1)
+
 mainloop()
